b061a88130699309c59dccac76fafcbaf7579bf1
topnn <50776627+topnn@users.noreply.github.com>
2020-11-07 15:24:35 -0600

    Update visualize_tfrecords_dataset.py
    Fixed missing `ConstantFeeder`, currently always using `training_dataset_circles` to pass a dataset to visualizer, regardless of the dataset type. TODO: fix transformer name.

--- pipelines/visualize_tfrecords_dataset.py
+++ pipelines/visualize_tfrecords_dataset.py
@@ -2,11 +2,15 @@ from argparse import Namespace
 from sklearn.pipeline import Pipeline
 from transformers.tf_reader import TFReader
 from transformers.visualizer_2d import Visualizer2D
+from transformers.constant_feeder import ConstantFeeder
+
 import os
 
 def load(args: Namespace) -> Pipeline:
     pipeline_steps = [
-        ('reader', TFReader(input_filename=os.path.join(args.input_tf_folder, args.input_tf_dataset))),
+        ('contents', ConstantFeeder({})),
+        ('training_dataset_circles', TFReader(input_filename=args.input_tf_dataset,
+                                              content_name='training_dataset')),
         ('visualizer', Visualizer2D(output_filename=os.path.join(args.output_plots_folder, "plot.html"))),
          ]
 
